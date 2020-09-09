272f30c6189252528471fb017065721cf46e2d55
Shawn Bierman <shawnbierman@icloud.com>
2020-09-09 16:29:28 -0500

    Initial Commit

---
+++ ChessProfiles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChessProfiles
+//
+//  Created by Shawn Bierman on 9/9/20.
+//  Copyright © 2020 Shawn Bierman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
