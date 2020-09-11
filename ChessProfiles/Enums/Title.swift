6d8a845b24198d0474f2c3435c47203041794021
Shawn Bierman <shawnbierman@icloud.com>
2020-09-11 14:45:17 -0500

    This offers more diversity in the Title enum

--- ChessProfiles/Enums/Title.swift
+++ ChessProfiles/Enums/Title.swift
@@ -8,18 +8,74 @@
 
 import Foundation
 
-enum Title: String, Codable {
+enum Title: String, Codable, CaseIterable {
     case GM, WGM, IM, WIM, FM, WFM, NM, WNM, CM, WCM
-//
-//    case GM = "Grandmaster"
-//    case WGM = "Woman Grandmaster"
-//    case IM = "International Master"
-//    case WIM = "Woman International Master"
-//    case FM = "FIDE Master"
-//    case WFM = "Woman FIDE Master"
-//    case NM = "National Master"
-//    case WNM = "Woman National Master"
-//    case CM = "Candidate Master"
-//    case WCM = "Woman Candidate Master"
-//    
 }
+
+extension Title {
+
+    var localizedName: String {
+
+        switch self {
+        case .GM:
+            return "Grandmaster"
+        case .WGM:
+            return "Woman Grandmaster"
+        case .IM:
+            return "International Master"
+        case .WIM:
+            return "Woman International Master"
+        case .FM:
+            return "FIDE Master"
+        case .WFM:
+            return "Woman FIDE Master"
+        case .NM:
+            return "National Master"
+        case .WNM:
+            return "Woman National Master"
+        case .CM:
+            return "Candidate Master"
+        case .WCM:
+            return "Woman Candidate Master"
+        }
+    }
+}
+
+// Dave DeLong's suggestion
+
+//struct Title: RawRepresentable, CaseIterable, Equatable, Hashable, Codable {
+//
+//    static let allCases = [Title.gm, .wgm]
+//
+//    static let gm = Title(rawValue: "gm", localizedName: "Grandmaster")
+//    static let wgm = Title(rawValue: "wgm", localizedName: "Woman Grandmaster")
+//
+//    let rawValue: String
+//    let localizedName: String
+//
+//    init?(rawValue: String) {
+//        guard let t = Self.allCases.first(where: { $0.rawValue == rawValue }) else { return nil }
+//        self = t
+//    }
+//
+//    private init(rawValue: String, localizedName: String) {
+//        self.rawValue = rawValue
+//        self.localizedName = localizedName
+//    }
+//
+//    static private func dataCorruptedError(in container: SingleValueDecodingContainer, debugDescription: String) -> DecodingError {
+//
+//    }
+//
+//    init(from decoder: Decoder) throws {
+//        let c = try decoder.singleValueContainer()
+//        let rawValue = try c.decode(String.self)
+//        guard let value = Title(rawValue: rawValue) else { throw DecodingError() }
+//        self = value
+//    }
+//
+//    func encode(to encoder: Encoder) throws {
+//        var c = try encoder.singleValueContainer()
+//        try c.encode(rawValue)
+//    }
+//}
