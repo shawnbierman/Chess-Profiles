129e6f4dc13ce0700d59f67e959434d60f74195e
Shawn Bierman <shawnbierman@icloud.com>
2020-09-11 19:43:35 -0500

    Country code model.

---
+++ ChessProfiles/Models/Country.swift
@@ -0,0 +1,14 @@
+//
+//  Country.swift
+//  ChessProfiles
+//
+//  Created by Shawn Bierman on 9/11/20.
+//  Copyright © 2020 Shawn Bierman. All rights reserved.
+//
+
+import Foundation
+
+struct Country: Codable {
+    let code: String
+    let name: String
+}
