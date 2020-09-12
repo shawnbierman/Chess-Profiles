f7b11443c1d753607e45282c4ca5e30048a6efed
Shawn Bierman <shawnbierman@icloud.com>
2020-09-11 19:44:21 -0500

    some logic that might be the making of a guessing game

--- ChessProfiles/Controllers/HomeViewController.swift
+++ ChessProfiles/Controllers/HomeViewController.swift
@@ -8,14 +8,59 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: BaseViewController {
+
+    var player: String? {
+        didSet {
+            
+            guard let player = player, player != "" else { return }
+
+            Network.shared.fetchProfile(for: player) { (result) in
+
+                switch result {
+
+                case .success(let profile):
+
+                    if let title = profile.title {
+                        print("Randomly chosen titled player is \"\(profile.username)\" with a title of \"\(title)\".")
+                    } else {
+                        print("Fetching again...")
+                        self.fetchRandomTitledPlayer()
+                    }
+
+                case .failure(let error):
+                    dump(error.localizedDescription)
+                }
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
     }
 
-    private func setup() {
-        view.backgroundColor = .systemBackground
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchRandomTitledPlayer()
+    }
+
+    private func fetchRandomTitledPlayer() {
+
+        // pick a random title
+        guard let title = Title.allCases.randomElement() else { return }
+
+        Network.shared.fetchPlayers(with: title) { (result) in
+
+            switch result {
+
+            case .success(let player):
+
+                // choose a random player with that title
+                self.player = player.players.randomElement()
+
+            case .failure(let error):
+                dump(error.localizedDescription)
+            }
+        }
     }
 }
